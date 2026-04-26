2c690c3afc892342c5e719f52142ccb866e6a2a6
Mario Junior <mariojljunior@gmail.com>
2026-04-26 14:16:37 -0400

    dia 3 tela de login

--- viajante-app/src/app/app.routes.ts
+++ viajante-app/src/app/app.routes.ts
@@ -1,7 +1,14 @@
 import { Routes } from '@angular/router';
 import { Home } from './components/home/home';
+import { Login } from './pages/login/login';
 
-export const routes: Routes = [{
+export const routes: Routes = [
+    {
     path: '',
     component: Home
-}];
+    },
+    {
+    path: 'login',
+    component: Login
+}
+];
--- viajante-app/src/app/components/home/home.ts
+++ viajante-app/src/app/components/home/home.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common'; // Importante para o [class]
 import { RouterLink } from '@angular/router'; // Importante para navegação interna
+import { DefaultLoginLayout } from '../default-login-layout/default-login-layout';
 
 @Component({
   selector: 'app-home',
   standalone: true, // Garanta que está como standalone
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule, RouterLink, DefaultLoginLayout],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
